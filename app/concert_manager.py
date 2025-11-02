d67d6f0ee8c39979c2f5d8c34e087551069bfdd1
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2025-11-02 16:18:02 +0000

    merge ConcertTrack and ConcertManager by removing MediaRelay

--- app/concert_manager.py
+++ app/concert_manager.py
@@ -1,51 +1,33 @@
-from aiortc.contrib.media import MediaPlayer, MediaRelay
+from aiortc.contrib.media import MediaPlayer
 from aiortc import MediaStreamTrack, MediaStreamError
+from av import AudioFrame
 from app.dependencies.media import get_media_path
-import asyncio
+from fractions import Fraction
+import numpy as np
 
-class ConcertTrack(MediaStreamTrack):
+class ConcertManager(MediaStreamTrack):
     kind = "audio"
 
-    def __init__(self, manager: ConcertManager):
-        super().__init__()
-        self._manager = manager
-        self._track: MediaStreamTrack = self._manager.next_track() # type: ignore
-        self._last = None
-
-    async def recv(self):
-        while True:
-            if not self._manager._started:
-                await asyncio.sleep(0.02)
-                if self._last is not None:
-                    return self._last
-            
-            try:
-                frame = await self._track.recv()
-                self._last = frame
-                return frame
-            except MediaStreamError:
-                next_track = self._manager.next_track()
-                if next_track is None:
-                    self.stop()
-                    raise MediaStreamError
-                self._track = next_track
-
-class ConcertManager:
     def __init__(self):
         self.file_dir = get_media_path()
-        
-        self._relay = MediaRelay()
         self._started = False
 
         self._tracks: list[MediaStreamTrack] = []
+        self._track_ptr = -1
         self.load_tracks()
-        self.track_ptr = -1
+        self._track = self.next_track()
+
+        self._sample_rate = 48000
+        self._channels = 2
+        self._samples = 960
     
     def next_track(self):
-        self.track_ptr += 1
-        if self.track_ptr < len(self._tracks):
-            return self._tracks[self.track_ptr]
-        return None
+        self._track_ptr += 1
+        if self._track_ptr < len(self._tracks):
+            return self._tracks[self._track_ptr]
+        else:
+            self.stop()
+            raise MediaStreamError("eof")
     
     def load_tracks(self):
         for file_path in self.file_dir.iterdir():
@@ -58,6 +40,18 @@ class ConcertManager:
     def stop(self):
         for t in self._tracks:
             t.stop()
-    
-    def create_track(self):
-        return self._relay.subscribe(ConcertTrack(self))
+            
+    async def recv(self):
+        if not self._started:
+            frame = AudioFrame(format="s16", layout="stereo", samples=self._samples)
+            frame.pts = 0
+            frame.time_base = Fraction(1, self._sample_rate)
+            silent_data = np.zeros((self._channels, self._samples), dtype=np.int16)
+            frame.planes[0].update(silent_data.tobytes())
+            return frame
+        
+        try:
+            return await self._track.recv()
+        except MediaStreamError:
+            self._track = self.next_track()            
+            return await self._track.recv()
