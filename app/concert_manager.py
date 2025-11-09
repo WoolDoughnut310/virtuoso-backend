f8d3c792cbbfb1071da2ebe1b6a28b6d86b5ffea
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2025-11-09 17:49:23 +0000

    connect concert manager to db for songs

--- app/concert_manager.py
+++ app/concert_manager.py
@@ -1,15 +1,23 @@
 from aiortc.contrib.media import MediaPlayer, MediaRelay
 from aiortc import MediaStreamTrack, MediaStreamError
 from av import AudioFrame
-from app.dependencies.media import get_media_path
+from app.models.concert import Song
 from fractions import Fraction
+from sqlmodel import Session, select
 import numpy as np
 
 class ConcertManager:
-    def __init__(self):
+    def __init__(self, id: int, db: Session):
         self._relay = MediaRelay()
         self._started = False
+        self.id = id
+        self.db = db
         self._concert_track = ConcertTrack(self)
+        
+        self.load_tracks = self._concert_track.load_tracks
+        self.load_track = self._concert_track.load_track
+
+        self.load_tracks()
     
     def create_track(self):
         return self._relay.subscribe(self._concert_track)
@@ -27,30 +35,50 @@ class ConcertTrack(MediaStreamTrack):
     def __init__(self, manager: ConcertManager):
         super().__init__()
         self.manager = manager
-        self.file_dir = get_media_path()
 
         self._tracks: list[MediaStreamTrack] = []
         self._track_ptr = -1
-        self.load_tracks()
-        self._track = self.next_track()
+        self._track: MediaStreamTrack | None = None
+
+        self._file_paths = {}
 
         self._sample_rate = 48000
         self._channels = 2
         self._samples = 960
         self._pts = 0
+
+    def get_track(self):
+        return self._tracks[self._track_ptr]
+
     
     def next_track(self):
         self._track_ptr += 1
         if self._track_ptr < len(self._tracks):
-            return self._tracks[self._track_ptr]
+            return self.get_track()
         else:
             self.stop()
             raise MediaStreamError("eof")
     
+    def load_track(self, song_id: int = -1, song: Song | None = None):
+        if song_id != -1:
+            song = self.manager.db.exec(select(Song).where(Song.id == song_id)).first()
+
+        if not song:
+            raise Exception("Song not found")
+        
+        if song.file_path in self._file_paths:
+            return
+        
+        self._file_paths[song.file_path] = True
+        player = MediaPlayer(song.file_path)
+        if player.audio:
+            self._tracks.append(player.audio)
+    
     def load_tracks(self):
-        for file_path in self.file_dir.iterdir():
-            player = MediaPlayer(file_path)
-            self._tracks.append(player.audio) # type: ignore
+        songs = self.manager.db.exec(select(Song).where(Song.id == self.manager.id)).all()
+
+        for song in songs:
+            self.load_track(song=song)
 
     def stop(self):
         for t in self._tracks:
@@ -58,6 +86,7 @@ class ConcertTrack(MediaStreamTrack):
             
     async def recv(self):
         if not self.manager._started:
+            print("silencing...")
             frame = AudioFrame(format="s16", layout="stereo", samples=self._samples)
             frame.pts = self._pts
             self._pts += self._samples
@@ -67,6 +96,8 @@ class ConcertTrack(MediaStreamTrack):
             return frame
         
         try:
+            if self._track is None:
+                self._track = self.next_track()
             return await self._track.recv()
         except MediaStreamError:
             self._track = self.next_track()
