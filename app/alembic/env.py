ebb6d292671368705b25ea2030b828e2eb3c5d84
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2025-11-09 17:47:50 +0000

    migrate db schema

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -8,6 +8,8 @@ from sqlmodel import SQLModel
 from app.config import settings
 from app.models.user import *
 from app.models.token import *
+from app.models.artist import *
+from app.models.concert import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
